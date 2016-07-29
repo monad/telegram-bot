f5f157a849d5d8590ce1b351c26bc340ca6a6b19
Lain <lain@lain.org.uk>
2016-07-29 06:04:39 +0100

    All non-cached result types implemented.

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -946,6 +946,26 @@ encode_subtypes!(InlineQueryResultVenue, 12,
                   9 => thumb_url, 10 => thumb_width, 11 => thumb_height]);
 
 // ---------------------------------------------------------------------------
+#[derive(Debug, PartialEq, Clone)]
+pub struct InlineQueryResultContact<K: InputMessageContent> {
+    pub _type: String,
+    pub id: String,
+    pub phone_number: String,
+    pub first_name: String,
+    pub last_name: Option<String>,
+    pub reply_markup: Option<InlineKeyboardMarkup>,
+    pub input_message_content: Option<K>,
+    pub thumb_url: Option<String>,
+    pub thumb_width: Option<Integer>,
+    pub thumb_height: Option<Integer>
+}
+
+encode_subtypes!(InlineQueryResultContact, 10,
+                 [0 => _type, 1 => id, 2 => phone_number, 3 => first_name],
+                 [4 => last_name, 5 => reply_markup, 6 => input_message_content,
+                  7 => thumb_url, 8 => thumb_width, 9 => thumb_height]);
+
+// ---------------------------------------------------------------------------
 #[derive(RustcEncodable, Debug, PartialEq, Clone)]
 pub struct InlineKeyboardButton {
     pub text: String,
